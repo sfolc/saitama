9c5a1cb6565d2fbe4b721aae0a113a22ec43b3ac
juneyah <1993kobayashi+sf@gmail.com>
2020-11-24 20:56:39 +0900

    test

--- js/saitama.js
+++ js/saitama.js
@@ -1,7 +1,47 @@
+function fillDiv(div, proportional) {
+  var currentWidth = div.outerWidth();
+  var currentHeight = div.outerHeight();
+
+  var availableHeight = window.innerHeight;
+  var availableWidth = window.innerWidth;
+
+  var scaleX = availableWidth / currentWidth;
+  var scaleY = availableHeight / currentHeight;
+
+  if (proportional) {
+    scaleX = Math.min(scaleX, scaleY);
+    scaleY = scaleX;
+  }
+
+  var translationX = Math.round((availableWidth - (currentWidth * scaleX)) / 2);
+  var translationY = Math.round((availableHeight - (currentHeight * scaleY)) / 2);
+
+  div.css({
+    "position": "fixed",
+    "left": "0px",
+    "top": "0px",
+    "-webkit-transform": "translate(" + translationX + "px, "
+                                      + translationY + "px) scale3d("
+                                      + scaleX + ", " + scaleY + ", 1)",
+    "-webkit-transform-origin": "0 0"
+  });
+}
+
 $(function(){
 
+  var saitama_main = $("#saitama-main");
+
+  //自動リサイズ
+  fillDiv(saitama_main, true);
+  if ("onorientationchange" in window) {
+    $(window).on("orientationchange", function() { setTimeout(function() { fillDiv(saitama_main, true); }, 500) });
+  } else if ("ondeviceorientation" in window) {
+    $(window).on("deviceorientation", function() { setTimeout(function() { fillDiv(saitama_main, true); }, 500) });
+  }
+  $(window).on("resize", function() { fillDiv(saitama_main, true); });;
+
   //右クリック（ロングタップ）メニュー抑制
-  $("#saitama-main").on("contextmenu",
+  saitama_main.on("contextmenu",
     function(event) {
       event.preventDefault();
       event.stopPropagation();
