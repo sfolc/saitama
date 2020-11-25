39d3bad94915264dba1d02d5cb7c7b92a3db2740
juneyah <1993kobayashi+sf@gmail.com>
2020-11-25 18:11:09 +0900

    hover

--- js/saitama.js
+++ js/saitama.js
@@ -59,7 +59,7 @@ $(function(){
   $(".hover")
     .on("touchstart", function(){
        $(this).addClass('_hover');
-    }).on("touchend", function(){
+    }).on("touchend", function(event){
       for (i=0; i<event.touches.length; i++) {
         if (document.elementFromPoint(event.touches[i].pageX, event.touches[i].pageY) === this) {
           return;
